7c83c43fc5c2af10b8309458b6fb5b6f06260075
nihalpandey4 <rockingnihal1305@gmail.com>
2021-01-12 23:33:01 +0530

    container added

--- src/compnents/App.js
+++ src/compnents/App.js
@@ -1,12 +1,14 @@
-
+import {Container} from "react-bootstrap"
 
 import SignUp from "./SignUp"
 
 const App=()=> {
   return (
-    <div className="App">
-      <SignUp/>
-    </div>
+    <Container className="d-flex justify-content-center align-items-center" style={{minHeight:"90vh"}}>
+      <div className="w-100" style={{maxWidth:"400px"}}>
+        <SignUp/>
+      </div>
+    </Container>
   );
 }
 
--- src/compnents/SignUp.js
+++ src/compnents/SignUp.js
@@ -1,7 +1,11 @@
-import React from "react"
+import React,{useRef} from "react"
 import {Card,Form,Button} from "react-bootstrap";
 
 const SignUp  = () =>{
+    const emailRef = useRef();
+    const passwordRef = useRef();
+    const confirmPasswordRef = useRef();
+
     return(
         <>
             <Card>
@@ -10,15 +14,15 @@ const SignUp  = () =>{
                     <Form>
                         <Form.Group>
                             <Form.Label>Email Address : </Form.Label>
-                            <Form.Control type="email" placeholder="Enter Email" />
+                            <Form.Control type="email" placeholder="Enter Email" ref= {emailRef} />
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>New Password : </Form.Label>
-                            <Form.Control type="password" placeholder="Enter password" />
+                            <Form.Control type="password" placeholder="Enter password" ref= {passwordRef} />
                         </Form.Group>
                         <Form.Group>
                             <Form.Label>Confirm Password : </Form.Label>
-                            <Form.Control type="password" placeholder="Confirm password" />
+                            <Form.Control type="password" placeholder="Confirm password" ref= {confirmPasswordRef} />
                         </Form.Group>
                         <Button type="submit" className="w-100">Submit</Button>
                     </Form>
