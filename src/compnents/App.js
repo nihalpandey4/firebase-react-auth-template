30409cfe21a9785da90795598cc754aee284c47c
nihalpandey4 <rockingnihal1305@gmail.com>
2021-01-13 17:04:35 +0530

    Auth provider added

--- src/compnents/App.js
+++ src/compnents/App.js
@@ -1,15 +1,20 @@
-import {Container} from "react-bootstrap"
+import { Container } from "react-bootstrap";
 
-import SignUp from "./SignUp"
+import SignUp from "./SignUp";
+import { AuthProvider } from "../contexts/AuthContext";
 
-const App=()=> {
+const App = () => {
   return (
-    <Container className="d-flex justify-content-center align-items-center" style={{minHeight:"90vh"}}>
-      <div className="w-100" style={{maxWidth:"400px"}}>
-        <SignUp/>
+    <Container
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "90vh" }}>
+      <div className="w-100" style={{ maxWidth: "400px" }}>
+        <AuthProvider>
+          <SignUp />
+        </AuthProvider>
       </div>
     </Container>
   );
-}
+};
 
 export default App;
