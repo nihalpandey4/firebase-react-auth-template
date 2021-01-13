167cdebea20de71162d2b3a3c354a7fc53f40b47
nihalpandey4 <rockingnihal1305@gmail.com>
2021-01-13 17:56:48 +0530

    logout functionality added

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,24 +1,37 @@
-import React from "react";
+import React,{useState} from "react";
 import { Link } from "react-router-dom";
-import { Card, Button } from "react-bootstrap";
+import { Card, Button,Alert } from "react-bootstrap";
 
-import {useAuth} from "../contexts/AuthContext"
+import { useAuth } from "../contexts/AuthContext";
 
 const Dashboard = () => {
-    const {currentUser} = useAuth()
+    const [errorMessage,setErrorMessage] = useState()
+  const { currentUser,logout } = useAuth();
+  const handleLogOut = async()=>{
+    try{
+        await logout();
+        setErrorMessage();
+    }
+    catch(error){
+        setErrorMessage(error.message);
+    }
+  }
   return (
     <>
       <Card>
         <Card.Body>
           <h2 className="text-center mb-3">Dashboard</h2>
+          {errorMessage&&<Alert variant="danger">{errorMessage}</Alert>}
           <strong>Email : </strong> {currentUser && currentUser.email}
           <div className="text-center w-100 mt-3">
-            <Link className="w-100 btn btn-lg btn-info"> Update profile</Link>
+            <Link className="w-100 btn btn-lg btn-info" to="/update-profile">
+              Update profile
+            </Link>
           </div>
         </Card.Body>
       </Card>
       <div className="text-center w-100 mt-3">
-        <Button variant="link"> Log Out</Button>
+        <Button variant="link" onClick ={()=>handleLogOut()}> Log Out</Button>
       </div>
     </>
   );
